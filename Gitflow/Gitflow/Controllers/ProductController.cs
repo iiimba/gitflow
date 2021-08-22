e2f4040a85c3eca9b86dbc262e951f6276ed2a57
Vlad Misnichenko <vmo@vava.cars>
2021-08-22 20:46:45 +0300

    Added ProductController

---
+++ Gitflow/Gitflow/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Gitflow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gitflow.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var product = new Product { Id = 1, Name = "Water", Price = 14.15m };
+
+            return Ok(product);
+        }
+    }
+}
--- Gitflow/Gitflow/Startup.cs
+++ Gitflow/Gitflow/Startup.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Gitflow
 {
@@ -16,6 +11,8 @@ namespace Gitflow
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +23,12 @@ namespace Gitflow
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gitflow"));
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
